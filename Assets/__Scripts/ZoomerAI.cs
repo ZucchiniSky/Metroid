f35af4116d65348f850318f5c19c5c9dbae3b94e
ZucchiniSky <scottbommarito@gmail.com>
2015-09-22 19:37:18 -0400

    first attempts for zoomer ai

--- Assets/__Scripts/ZoomerAI.cs
+++ Assets/__Scripts/ZoomerAI.cs
@@ -1,26 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RipperAI : MonoBehaviour {
-    
-    public bool wallLeft = false;
-    public bool wallRight = false;
-    public float velX = 2f;
-    public bool facingRight = true;
+public enum direction
+{
+    UP,
+    LEFT,
+    RIGHT,
+    DOWN
+}
+
+public class ZoomerAI : MonoBehaviour {
+    public int hp;
 
     public bool _______;
+    public float speed = 2f;
+    public direction dir = direction.RIGHT;
     public Rigidbody rigid;
     public CapsuleCollider body;
     public int groundPhysicsLayerMask;
     Quaternion turnLeft = Quaternion.Euler(0, 180, 0);
+    Quaternion turnUp = Quaternion.Euler(0, 90, 0);
+    Quaternion turnDown = Quaternion.Euler(0, 270, 0);
+
     // Use this for initialization
     void Start () {
+        int initDir = (int) Mathf.Round(Random.Range(0, 1));
+        if (initDir == 0)
+        {
+            dir = direction.LEFT;
+        } else
+        {
+            dir = direction.RIGHT;
+        }
         rigid = GetComponent<Rigidbody>();
         body = GetComponent<CapsuleCollider>();
         groundPhysicsLayerMask = LayerMask.GetMask("Ground");
     }
-
-    void FixedUpdate() {
+	
+	// Update is called once per frame
+	void Update () {
         int x = Mathf.RoundToInt(CameraFollow.S.transform.position.x);
         int y = Mathf.RoundToInt(CameraFollow.S.transform.position.y);
         int i0 = x - 9;
@@ -28,20 +46,31 @@ public class RipperAI : MonoBehaviour {
         int j0 = y - 9;
         int j1 = y + 9;
         Vector3 vel = rigid.velocity;
-        
-        if(transform.position.x < i0-20 || transform.position.x > i1+20
-            || transform.position.y < j0-20 || transform.position.y > j1 + 20)
+
+        if (transform.position.x < i0 - 20 || transform.position.x > i1 + 20
+            || transform.position.y < j0 - 20 || transform.position.y > j1 + 20)
         {
             Destroy(gameObject);
         }
         else if (transform.position.x < i0 || transform.position.x > i1
             || transform.position.y < j0 || transform.position.y > j1)
         {
-            velX = 0;
+            speed = 0;
         }
         else
         {
-            
+            Vector2 checkLoc = body.transform.position;
+            switch (dir)
+            {
+                case direction.LEFT:
+                    break;
+                case direction.RIGHT:
+                    break;
+                case direction.UP:
+                    break;
+                case direction.DOWN:
+                    break;
+            }
             Vector3 checkLoc1 = body.transform.position + Vector3.left * (body.height * 0.45f);
             wallLeft = Physics.Raycast(checkLoc1, Vector3.left, body.radius, groundPhysicsLayerMask);
             Vector3 checkLoc2 = body.transform.position + Vector3.right * (body.height * 0.45f);
@@ -69,7 +98,25 @@ public class RipperAI : MonoBehaviour {
                 velX = -2f;
             }
         }
-        vel.x = velX;
+        switch (dir)
+        {
+            case direction.LEFT:
+                vel.x = -speed;
+                transform.rotation = turnLeft;
+                break;
+            case direction.RIGHT:
+                vel.x = speed;
+                transform.rotation = Quaternion.identity;
+                break;
+            case direction.UP:
+                vel.y = speed;
+                transform.rotation = turnUp;
+                break;
+            case direction.DOWN:
+                vel.y = -speed;
+                transform.rotation = turnDown;
+                break;
+        }
         rigid.velocity = vel;
     }
 }
