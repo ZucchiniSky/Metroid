400ac3a5884db441c44ba6d4b3f59abcf5f06bd4
ZucchiniSky <scottbommarito@gmail.com>
2015-09-23 04:35:27 -0400

    added missile pickups to zeb and red zoomer
    Conflicts:
    	Assets/Textures and Materials/MetroidFullMapData.txt
    	Assets/__Scripts/ZoomerAI.cs

--- Assets/__Scripts/ZebAI.cs
+++ Assets/__Scripts/ZebAI.cs
@@ -18,9 +18,10 @@ public class ZebAI : MonoBehaviour {
     private bool right = true;
     private int hp = 2;
     private int shot = 0;
+    public GameObject energyPrefab, missilePrefab;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rigid = GetComponent<Rigidbody>();
         if (Samus.S.transform.position.x < gameObject.transform.position.x)
         {
@@ -94,6 +95,17 @@ public class ZebAI : MonoBehaviour {
             shot = 3;
             if (hp <= 0)
             {
+                int initDir = (int)Mathf.Round(Random.Range(0, 3));
+                if (initDir == 0)
+                {
+                    GameObject go = Instantiate(energyPrefab);
+                    go.transform.position = transform.position;
+                }
+                else if (initDir == 1 && Samus.S.hasMissiles)
+                {
+                    GameObject go = Instantiate(missilePrefab);
+                    go.transform.position = transform.position;
+                }
                 Destroy(gameObject);
             }
         }
