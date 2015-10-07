5aaca73b4c23718ef2b52b9f38f97a75e1ce376d
ZucchiniSky <scottbommarito@gmail.com>
2015-10-07 00:07:33 -0400

    rocks shoot up

--- Assets/__Scripts/scottAI.cs
+++ Assets/__Scripts/scottAI.cs
@@ -14,14 +14,18 @@ public class scottAI : MonoBehaviour {
 	void FixedUpdate () {
         int x = Mathf.RoundToInt(CameraScrolling.S.transform.position.x);
         int y = Mathf.RoundToInt(CameraScrolling.S.transform.position.y);
-        int i0 = x - 18;
-        int i1 = x + 18;
-        int j0 = y - 18;
-        int j1 = y + 18;
+        int i0 = x - 19;
+        int i1 = x + 19;
+        int j0 = y - 19;
+        int j1 = y + 19;
         if (transform.position.x < i0 || transform.position.x > i1
             || transform.position.y < j0 || transform.position.y > j1)
         {
             Destroy(gameObject);
+        }
+        if (transform.position.x == Mathf.Round(transform.position.x))
+        {
+            transform.position = transform.position + new Vector3(-.05f, 0f, 0f);
         }
     }
 
@@ -31,22 +35,15 @@ public class scottAI : MonoBehaviour {
         {
             float charge = other.GetComponent<SamusBullet>().charge;
             Vector3 vel = rigid.velocity;
-            float bulletVel = other.GetComponent<Rigidbody>().velocity.x;
-            bool right;
-            if (bulletVel != 0)
+            float bulletVelX = other.GetComponent<Rigidbody>().velocity.x;
+            float bulletVelY = other.GetComponent<Rigidbody>().velocity.y;
+            if (bulletVelX != 0)
             {
-                right = bulletVel > 0;
-            } else
-            {
-                right = other.transform.position.x < transform.position.x;
+                vel.x += (bulletVelX > 0 ? 1 : -1) * charge * 1f;
             }
-            if (right)
+            if (bulletVelY != 0)
             {
-                vel.x += charge * 1f;
-            }
-            else
-            {
-                vel.x -= charge * 1f;
+                vel.y += (bulletVelY > 0 ? 1 : -1) * charge * 1f;
             }
             rigid.velocity = vel;
         }
